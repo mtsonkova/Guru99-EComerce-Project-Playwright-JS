ce751088abc3c101f73044fe7dd79ab9d27bd376
mtsonkova <mtsonkova950@gmail.com>
2024-07-16 00:41:55 +0300

    In progress

--- pagepbjects/NavigationElements.js
+++ pagepbjects/NavigationElements.js
@@ -8,6 +8,14 @@ class NavigationElements{
         this.comparedItemsList = page.locator('ol#recently-compared-items');
         this.searchBox = page.locator('.search');
         this.newsLetterBox = page.locator('input[type="email"]');
+        this.btnSubscribe = page.getByRole('button', {name: "Subscribe"});
+        this.aboutUs = page.getByRole('a', {name : 'About Us'});
+        this.contactUs = page.getByRole('a', {name : 'Contact Us'})
+        this.customerService = page.getByRole('a', {name : 'Customer Service'})
+        this.privacyPolicy = page.getByRole('a', {name : 'Privacy Policy'})
+        this.myAccount = page.getByRole('a', {name : 'My Account'})
+        this.ordersAndReturns = page.getByRole('a', {name : 'Orders and Returns'})
+        this.btnAccount = page.locator('.skip-account span.label');
         
     }
 }
