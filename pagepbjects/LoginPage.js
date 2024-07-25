07784f9449da67f69d69126c848d61a83321407e
mtsonkova <mtsonkova950@gmail.com>
2024-07-25 19:41:04 +0300

    LoginPage.js file created:

---
+++ pagepbjects/LoginPage.js
@@ -0,0 +1,32 @@
+class LoginPage{
+    constructor(page){
+        this.page = page;
+        this.email = page.locator('#email');
+        this.password = page.locator('#pass');
+        this.btnLogin = page.locator('#send2');
+        this.forgotPassword = page.locator('a.f-left');
+        this.createAccount = page.locator('.buttons-set a');
+    }
+
+    async enterEmail(email) {
+        await this.email.fill(email);
+    }
+
+    async enterPassword(pass) {
+        await this.password.fill(pass);
+    }
+
+    async clickBtnLogin() {
+        await this.btnLogin.click();
+    }
+
+    async clickForgotPassword() {
+        await this.forgotPassword.click();
+    }
+
+    async clickCreateAccount(){
+        await this.createAccount.click();
+    }
+}
+
+module.exports = {LoginPage};
