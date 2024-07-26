7c28bd8c91c6444f3535e6deb943b11b2e43c76e
mtsonkova <mtsonkova950@gmail.com>
2024-07-26 22:13:48 +0300

    Done writing the CreateAccountPage.js

--- pagepbjects/CreateAccountPage.js
+++ pagepbjects/CreateAccountPage.js
@@ -1,4 +1,4 @@
-class CreateAccountPage{
+class CreateAccountPage {
     constructor(page) {
         this.page = page;
         this.firstName = page.locator('#firstname');
@@ -7,43 +7,79 @@ class CreateAccountPage{
         this.email = page.locator('#email_address');
         this.password = page.locator('#password');
         this.confirmPassword = page.locator('#confirmation');
-        this.signUpForNewsletter = page.locator('#is_subscribed');  
-        this.btnRegister = page.getByRole('button', {name: 'Register'});
-      }
+        this.signUpForNewsletter = page.locator('#is_subscribed');
+        this.btnRegister = page.getByRole('button', { name: 'Register' });
+    }
 
-      async enterFirstName(firstName) {
+    async enterFirstName(firstName) {
         await this.firstName.fill(firstName);
-      }
+    }
 
-      async enterMiddleName(middleName) {
+    async enterMiddleName(middleName) {
         await this.middleName.fill(middleName);
-      }
+    }
 
-      async enterLastName(lastName) {
+    async enterLastName(lastName) {
         await this.lastName.fill(lastName);
-      }
+    }
 
-      async enterEmail(email) {
+    async enterEmail(email) {
         await this.email.fill(email);
-      }
+    }
 
-      async enterPassword(pass) {
+    async enterPassword(pass) {
         await this.password.fill(pass);
-      }
+    }
 
-      async enterConfirmPassword(pass) {
+    async enterConfirmPassword(pass) {
         await this.confirmPassword.fill(pass);
-      }
+    }
 
-      async clickSignForNewsLetter() {
+    async clickSignForNewsLetter() {
         await this.signUpForNewsletter.check();
-      }
+    }
 
-      async clickOnRegisterBtn() {
+    async clickOnRegisterBtn() {
         await this.btnRegister.click();
-      }
+    }
 
-      async registerWithValidDataAllFields(firstName, middleName, lastName, password) {
+    async registerWithValidDataAllFieldsSignedForNews(firstName, middleName, lastName, email, password) {
+        await this.enterFirstName(firstName);
+        await this.enterMiddleName(middleName);
+        await this.enterLastName(lastName);
+        await this.enterEmail(email);
+        await this.enterPassword(password);
+        await this.enterConfirmPassword(password);
+        await this.clickSignForNewsLetter();
+        await this.clickOnRegisterBtn();
+    }
 
-      }
+    async registerWithValidDataAllFieldsNotSignedForNews(firstName, middleName, lastName, email, password) {
+        await this.enterFirstName(firstName);
+        await this.enterMiddleName(middleName);
+        await this.enterLastName(lastName);
+        await this.enterEmail(email);
+        await this.enterPassword(password);
+        await this.enterConfirmPassword(password);
+        await this.clickOnRegisterBtn();
+    }
+
+    async registerWithValidDataRequiredFieldsOnlySignedForNews(firstName,  lastName, email, password) {
+        await this.enterFirstName(firstName);
+        await this.enterLastName(lastName);
+        await this.enterEmail(email);
+        await this.enterPassword(password);
+        await this.enterConfirmPassword(password);
+        await this.clickSignForNewsLetter();
+        await this.clickOnRegisterBtn();
+    }
+
+    async registerWithValidDataRequiredFieldsOnlyNotSignedForNews(firstName,  lastName, email, password) {
+        await this.enterFirstName(firstName);
+        await this.enterLastName(lastName);
+        await this.enterEmail(email);
+        await this.enterPassword(password);
+        await this.enterConfirmPassword(password);
+        await this.clickOnRegisterBtn();
+    }
 }
