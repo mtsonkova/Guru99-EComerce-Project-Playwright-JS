eaaf79fd4d255f94c4a765ab0b4d17b0444bbd05
mtsonkova <mtsonkova950@gmail.com>
2024-07-29 23:16:54 +0300

    In progress

--- pagepbjects/CheckoutPage.js
+++ pagepbjects/CheckoutPage.js
@@ -26,6 +26,41 @@ class CheckoutPage{
         this.zipPostcode = page.locator('#billing:postcode');
         this.country = page.locator('#billing:country_id');
         this.phone = page.locator('#billing:telephone');
+        this.fax = page.locator('#billing:fax');
+        this.shipToAddressCheckbox = page.locator('#billing:use_for_shipping_yes');
+        this.shipToDifferentAddresCheckbox = page.locator('billing:use_for_shipping_no');
+        this.billingContinueBtn = page.getByRole('button', {name:'Continue'});
+        
+        //Register user during purchase
+        this.password = page.locator('#billing:customer_password');
+        this.confirmPassword = page.locator('#billing:confirm_password');
+
+        //Shipping information form
+        this.shippingFirstName = page.locator('#shipping:firstname');
+        this.shippingMiddleName = page.locator('#shipping:middlename');
+        this.shippingLastName = page.locator('#shipping:lastname');
+        this.shippingCompany = page.locator('#shipping:company');
+        this.shippingAddress = page.locator('#shipping:street1');
+        this.shippingAddress2 = page.locator('#shipping:street2');
+        this.shippingCity = page.locator('#shipping:city');
+        this.shippingStateProvince = page.locator('#shipping:region_id');
+        this.shippingZIPOrPostal = page.locator('#shipping:postcode');
+        this.shippingCountry = page.locator('#shipping:country_id');
+        this.shippingPhone = page.locator('#shipping:telephone');
+        this.shippingFax = page.locator('#shipping:fax');
+
+        this.useBillingAddressCheckbox = page.locator('#shipping:same_as_billing');
+        this.shippingContinueBtn = page.getByRole('button', {name:'Continue'});
+
+        //Shipping method
+        this.tax = page.locator('dd label span.price');
+
+
+        this.btnContinue = page.locatore('button:visible :text("Continue")');
+
+        //Payment method
+        this.creditCardRadioBtn = page.locator('#p_method_ccsave');
+        this.cashMoneyRadioBtn = page.locator('#p_method_checkmo');
 
 
 
