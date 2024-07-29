8e5085aad6c5ac10b0b5a4c65f98b3abc49fe6e8
mtsonkova <mtsonkova950@gmail.com>
2024-07-29 22:02:52 +0300

    In progress

--- pagepbjects/CheckoutPage.js
+++ pagepbjects/CheckoutPage.js
@@ -8,6 +8,25 @@ class CheckoutPage{
          this.loginBtn = page.getByRole('button', {name: 'Login'});
          this.forgotPasswordLink = page.getByRole('a', {name: 'Forgot your password?'});
 
+        // Checkout as guest or register
+        this.checkoutAsGuest = page.locaotr('#login:guest');
+        this.registerAndCheckout = page.locator('#login:register');
+        this.continueBtn = page.locator('#onepage-guest-register-button');
+
+        //Billing information
+        this.firstName = page.locator('#billing:firstname');
+        this.middleName = page.locator('#billing:middlename');
+        this.lastName = page.locator('#billing:lastname');
+        this.company = page.locator('#billing:company');
+        this.email = page.locator('#billing:email');
+        this.address = page.locator('#billing:street1');
+        this.secondAddress = page.locator('#billing:street2');
+        this.city = page.locator('#billing:city');
+        this.stateOrProvince = page.locator('#billing:region_id');
+        this.zipPostcode = page.locator('#billing:postcode');
+        this.country = page.locator('#billing:country_id');
+        this.phone = page.locator('#billing:telephone');
+
 
 
     }
@@ -16,5 +35,15 @@ class CheckoutPage{
         await this.loginEmail.fill(email);
         await this.loginPassword.fill(password);
         await this.loginBtn.click();
+    }
+
+    async selectCheckoutAsGuest() {
+        await this.checkoutAsGuest.check();
+        await this.continueBtn.click();
+    }
+
+    async selectRegisterAndCheckout() {
+        await this.registerAndCheckout.check();
+        await this.continueBtn.click();
     }
 }
