a20f470281f48e2b7ac2e6f8ed248775639ec411
mtsonkova <mtsonkova950@gmail.com>
2024-10-05 16:20:36 +0300

    Test case 7 is completed

--- tests/E2ETests.spec.js
+++ tests/E2ETests.spec.js
@@ -3,21 +3,24 @@ const { chromium } = require('playwright');
 
 const { LandingPage } = require('../pagepbjects/LandingPage');
 const navigation = require('../testFiles/navigations.json');
-const {HeaderNav} = require('../utils/HeaderNav');
+const { HeaderNav } = require('../utils/HeaderNav');
 const { ReusableProductsFunctions } = require('../utils/ReusableProductsFunctions');
 const { ProductsPage } = require('../pagepbjects/ProductsPage');
 const { ProductInformationPage } = require('../pagepbjects/ProductInformationPage');
-const { CartPage} = require('../pagepbjects/CartPage');
+const { CartPage } = require('../pagepbjects/CartPage');
 const { TIMEOUT } = require('dns');
-const {ReusableFunctions} = require('../utils/ReusableFunctions');
-const {NavigationElements} = require('../utils/NavigationElements');
-const {CompareProductsPage} = require('../pagepbjects/CompareProductsPage');
+const { ReusableFunctions } = require('../utils/ReusableFunctions');
+const { NavigationElements } = require('../utils/NavigationElements');
+const { CompareProductsPage } = require('../pagepbjects/CompareProductsPage');
 const { LoginPage } = require('../pagepbjects/LoginPage');
-const {MyWishlistPage} = require('../pagepbjects/MyWishlistPage');
-const {ShareYourWishListPage} = require('../pagepbjects/ShareYourWishlistPage');
-const {CheckoutPage} = require('../pagepbjects/CheckoutPage');
-const {PlacedOrderPage} = require('../pagepbjects/PlacedOrderPage');
-const {MyOrdersPage} = require('../pagepbjects/MyOrdersPage');
+const { MyWishlistPage } = require('../pagepbjects/MyWishlistPage');
+const { ShareYourWishListPage } = require('../pagepbjects/ShareYourWishlistPage');
+const { CheckoutPage } = require('../pagepbjects/CheckoutPage');
+const { PlacedOrderPage } = require('../pagepbjects/PlacedOrderPage');
+const { MyOrdersPage } = require('../pagepbjects/MyOrdersPage');
+
+const fs = require('fs');
+const path = require('path');
 
 let browser;
 let context;
@@ -46,7 +49,7 @@ describe('End to End Tests', async () => {
     });
 
     beforeEach(async () => {
-        context = await browser.newContext();
+        context = await browser.newContext({ acceptDownloads: true });
         page = await context.newPage();
         await page.goto('/');
         headerNav = new HeaderNav(page);
@@ -59,51 +62,51 @@ describe('End to End Tests', async () => {
         checkoutPage = new CheckoutPage(page);
         placedOrder = new PlacedOrderPage(page);
         orders = new MyOrdersPage(page);
-        
+
     });
 
     afterEach(async () => {
         await page.close();
         await context.close();
     });
-    
+
     describe('Guru99 Tests', async () => {
-        test('Sort mobile phones by price ascending and check if they are sorted correctly', async() => {
-            await headerNav.clickOnMobile();  
+        test('Sort mobile phones by price ascending and check if they are sorted correctly', async () => {
+            await headerNav.clickOnMobile();
             await page.waitForURL('http://live.techpanda.org/index.php/mobile.html');
             let devices = await productsPage.getAllDevicesWithNameAndPrice();
             let sortedDevices = devices.sort((a, b) => a.price - b.price);
-          
+
             //click sort by Price
-            await productsPage.sortByOption('http://live.techpanda.org/index.php/mobile.html?dir=asc&order=price'); 
+            await productsPage.sortByOption('http://live.techpanda.org/index.php/mobile.html?dir=asc&order=price');
             await page.waitForURL('http://live.techpanda.org/index.php/mobile.html?dir=asc&order=price');
             let sortedDevicesByPrice = await productsPage.getAllDevicesWithNameAndPrice();
-           
+
             let comparisonResult = ReusableFunctions.compareTwoProductArrays(sortedDevices, sortedDevicesByPrice);
-         
+
             expect(comparisonResult).toBeTruthy();
-            
+
         });
 
-        test('Sort mobile phones by name from A to Z and check if they are sorted correctly', async() => {
-            await headerNav.clickOnMobile();  
+        test('Sort mobile phones by name from A to Z and check if they are sorted correctly', async () => {
+            await headerNav.clickOnMobile();
             await page.waitForURL('http://live.techpanda.org/index.php/mobile.html');
             let devices = await productsPage.getAllDevicesWithNameAndPrice();
             let sortedDevices = devices.sort((a, b) => (a.name).localeCompare(b.name));
 
             await productsPage.sortByOption('http://live.techpanda.org/index.php/mobile.html?dir=asc&order=name');
-           
+
             await page.waitForURL('http://live.techpanda.org/index.php/mobile.html?dir=asc&order=name')
-           
-           let sortedDevicesByName = await productsPage.getAllDevicesWithNameAndPrice();
-           
-           let result = ReusableFunctions.compareTwoProductArrays(sortedDevices, sortedDevicesByName);
-            
-           expect(result).toBeTruthy();
+
+            let sortedDevicesByName = await productsPage.getAllDevicesWithNameAndPrice();
+
+            let result = ReusableFunctions.compareTwoProductArrays(sortedDevices, sortedDevicesByName);
+
+            expect(result).toBeTruthy();
         });
 
-        test('Verify if cost of product on products page and details page are the same', async() => {
-            await headerNav.clickOnMobile();  
+        test('Verify if cost of product on products page and details page are the same', async () => {
+            await headerNav.clickOnMobile();
             await page.waitForURL('http://live.techpanda.org/index.php/mobile.html');
             let device = await productsPage.getDeviceByName('Sony Xperia');
             let productPrice = await device.locator('.price').textContent();
@@ -111,13 +114,13 @@ describe('End to End Tests', async () => {
 
             await page.locator('a:has-text("Sony Xperia")').click();
             let priceFromDetailsPage = await productInformationPage.getProductPrice();
-           
+
             expect(productPrice).toEqual(priceFromDetailsPage);
 
         });
 
-        test('Try to purchase more qty of a product than the available in the store', async() => {
-            await headerNav.clickOnMobile();  
+        test('Try to purchase more qty of a product than the available in the store', async () => {
+            await headerNav.clickOnMobile();
             await page.waitForURL('http://live.techpanda.org/index.php/mobile.html');
             //let device = await productsPage.getDeviceByName('Sony Xperia');
             //await productFunctions.clickAddToCart(device);
@@ -126,17 +129,17 @@ describe('End to End Tests', async () => {
             let result = await cartPage.changeProductQty('Sony Xperia', '1000');
             let cartErrMsg = result.cartErrMsg;
             let productErrMsg = result.productErrMsg;
-     
-          await expect(result === undefined).toBeFalsy();
-          await expect(productErrMsg === undefined).toBeFalsy();
-          await expect(cartErrMsg === undefined).toBeFalsy();
-          await expect(productErrMsg).toEqual('* The maximum quantity allowed for purchase is 500.');
-          await expect(cartErrMsg).toEqual('Some of the products cannot be ordered in requested quantity.');
-               
+
+            await expect(result === undefined).toBeFalsy();
+            await expect(productErrMsg === undefined).toBeFalsy();
+            await expect(cartErrMsg === undefined).toBeFalsy();
+            await expect(productErrMsg).toEqual('* The maximum quantity allowed for purchase is 500.');
+            await expect(cartErrMsg).toEqual('Some of the products cannot be ordered in requested quantity.');
+
         });
 
-        test('Remove all products from shopping cart and expect Shopping cart is empty message', async() => {
-            await headerNav.clickOnMobile();  
+        test('Remove all products from shopping cart and expect Shopping cart is empty message', async () => {
+            await headerNav.clickOnMobile();
             await page.waitForURL('http://live.techpanda.org/index.php/mobile.html');
             await productsPage.addProductToCart('IPhone');
             await page.waitForURL('http://live.techpanda.org/index.php/checkout/cart/');
@@ -145,10 +148,10 @@ describe('End to End Tests', async () => {
             await expect(emptyCartTitle).toEqual('Shopping Cart is Empty');
         });
 
-        test('Verify that you are able to compare two products', async() => {
-            await headerNav.clickOnMobile();  
+        test('Verify that you are able to compare two products', async () => {
+            await headerNav.clickOnMobile();
             await page.waitForURL('http://live.techpanda.org/index.php/mobile.html');
-            for(let i = 0; i < devicesArr.length; i++) {
+            for (let i = 0; i < devicesArr.length; i++) {
                 let currentDeviceName = devicesArr[i];
                 let device = await productsPage.getDeviceByName(currentDeviceName);
                 await productFunctions.clickAddToCompare(device);
@@ -156,78 +159,106 @@ describe('End to End Tests', async () => {
 
             const [popupPage] = await Promise.all([
                 context.waitForEvent('page'),
-                page.getByRole('button', {name: 'Compare'}).click()
+                page.getByRole('button', { name: 'Compare' }).click()
             ]);
             comparedProducts = new CompareProductsPage(popupPage);
 
             let comparedDevicesNames = await comparedProducts.getProductsNames();
-            
+
             await popupPage.close();
             await page.waitForURL('http://live.techpanda.org/index.php/mobile.html');
-                    
-          
-           await expect(devicesArr.length === comparedDevicesNames.length).toBeTruthy();
-           devicesArr.forEach(device => expect(comparedDevicesNames.includes(device)).toBeTruthy);          
-           
+
+
+            await expect(devicesArr.length === comparedDevicesNames.length).toBeTruthy();
+            devicesArr.forEach(device => expect(comparedDevicesNames.includes(device)).toBeTruthy);
+
         });
 
-        test('Login and share wishlist to other people', async() => {
+        test('Login and share wishlist to other people', async () => {
             await headerNav.clickOnAccount();
             await headerNav.clickOnLogIn();
             await page.waitForURL('http://live.techpanda.org/index.php/customer/account/login/');
-          
+
             let logInUser = new LoginPage(page);
             await logInUser.loginWithValidCredentials('samgreen@test.qa', 'password');
-            await page.getByRole('link', {name: 'MY WISHLIST'}).click();
+            await page.getByRole('link', { name: 'MY WISHLIST' }).click();
             let myWishList = new MyWishlistPage(page);
             await myWishList.clickShareWishListBtn();
-            let shareWishListPage = new ShareYourWishListPage(page);   
+            let shareWishListPage = new ShareYourWishListPage(page);
             await shareWishListPage.ShareYourWishlist(emailsArr, 'lorem ipsum');
             navigationElements = new NavigationElements(page);
             let sharedWishListMsg = await navigationElements.getSuccessMsg();
             console.log(sharedWishListMsg)
-            await expect(sharedWishListMsg === 'Your Wishlist has been shared.').toBeTruthy();  
+            await expect(sharedWishListMsg === 'Your Wishlist has been shared.').toBeTruthy();
         });
 
-        test('Purchase products from My Wish List', async() => {
+        test('Purchase products from My Wish List', async () => {
             await headerNav.clickOnAccount();
             await headerNav.clickOnLogIn();
             await page.waitForURL('http://live.techpanda.org/index.php/customer/account/login/');
-          
+
             let logInUser = new LoginPage(page);
             await logInUser.loginWithValidCredentials('samgreen@test.qa', 'password');
-            await headerNav.clickOnMobile();  
+            await headerNav.clickOnMobile();
             await page.waitForURL('http://live.techpanda.org/index.php/mobile.html');
-           //add product to wishlist
-           await productsPage.addProductToWishlist('Sony Xperia');
-          
+            //add product to wishlist
+            await productsPage.addProductToWishlist('Sony Xperia');
+
             let myWishList = new MyWishlistPage(page);
             await myWishList.clickOnAddToCart();
-         
+
             await cartPage.clickOnProceedToCheckoutBtn();
-         
+
             await checkoutPage.checkoutAsLoggedInUser();
-            
+
             await page.waitForURL('http://live.techpanda.org/index.php/checkout/onepage/success/');
             let text = await placedOrder.getOrderReceivedTitle();
             let orderId = await placedOrder.getOrderId();
-           
+
             await expect(text === 'Your order has been received.').toBeTruthy();
             await expect(orderId === '').toBeFalsy();
         });
 
-        test.only('Save previousli placed order as a pdf file', async() => {
+        test.only('Save previously placed order as a pdf file', async () => {
             await headerNav.clickOnAccount();
             await headerNav.clickOnLogIn();
             await page.waitForURL('http://live.techpanda.org/index.php/customer/account/login/');
-          
+
             let logInUser = new LoginPage(page);
             await logInUser.loginWithValidCredentials('samgreen@test.qa', 'password');
-            await page.getByRole('link', {name: 'MY ORDERS'}).click();
+            await page.getByRole('link', { name: 'MY ORDERS' }).click();
             await orders.clickOnViewOrder();
+            let orderHeader = await page.locator('h1').textContent();
+            let arr = await orderHeader.split(' ');
+            let orderId = arr[1].slice(1);
 
+            // generate expected file name
+            let fileName = `Print Order # ${orderId}.pdf`;
+            let folderPath = path.join(__dirname, '..') + '\\Downloads';
+            const filePath = path.join(folderPath, fileName);
+            
+           // Generate the PDF
+            await page.pdf({
+              path: filePath,  // File path where the PDF will be saved
+              format: 'A4',  // Page format (can be 'A4', 'Letter', etc.)
+              printBackground: true,  // Whether to include background graphics
+              margin: {
+                top: '1in',
+                right: '1in',
+                bottom: '1in',
+                left: '1in'
+              }
+            });
+          
+           
+            let hasOrder = false;
+
+            if(fs.existsSync(filePath)) {
+               hasOrder = true;
+              } 
+             await expect(hasOrder).toBeTruthy();
         })
 
     });
-   
+
 });
